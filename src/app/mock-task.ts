22d6b0776b9c84f9a6103a86f77e134f358ddd94
Ilich Betancourt Rangel <ilichbr@gmail.com>
2022-04-20 17:25:31 -0300

    Agregando tareas en JSON, Mockeado en archivo ts

---
+++ src/app/mock-task.ts
@@ -0,0 +1,26 @@
+import { Task } from "./Task"
+export const TASK: Task []=[
+    {
+        id: 1,
+        text: "Terminar esta App",
+        dia: "Hoy 20 de Abril",
+        recordatorio: true
+
+    },
+    {
+        id: 2,
+        text: "Continuar modulo 8 ",
+        dia: "Hoy 20 de Abril",
+        recordatorio: false
+
+    },
+    {
+        id: 3,
+        text: "Actualizar proyecto con lo aprendido hoy",
+        dia: "Hoy 20 de Abril 23:00",
+        recordatorio: true
+
+    },
+    
+
+]
