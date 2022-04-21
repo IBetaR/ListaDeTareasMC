8731f5e365fbd1dfe97e57c7c6e5412e164b9e71
Ilich Betancourt Rangel <ilichbr@gmail.com>
2022-04-20 22:01:06 -0300

    Creación de DB con JSON server Lista de tareas MC

---
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { TASK } from 'src/app/mock-task';
+import {  faTimeline } from '@fortawesome/free-solid-svg-icons';
+
+
+@Component({
+  selector: 'app-tasks-item',
+  templateUrl: './tasks-item.component.html',
+  styleUrls: ['./tasks-item.component.css']
+})
+export class TasksItemComponent implements OnInit {
+  @Input() task: Task = TASK[0]
+  faTimeline = faTimeline;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
